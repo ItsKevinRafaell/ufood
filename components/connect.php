bbe0f5e3933c1f158cb9f15e47e551b2002181ec
KepinNiBoss <punya.kepin@gmail.com>
2022-11-21 12:45:22 +0800

    deplpoy

---
+++ components/connect.php
@@ -0,0 +1,9 @@
+<?php
+
+$db_name = 'mysql:host=sql112.epizy.com;dbname=epiz_33043861_food_db';
+$user_name = 'epiz_33043861';
+$user_password = '5h18ezRYeC9';
+
+$conn = new PDO($db_name, $user_name, $user_password);
+
+?>
